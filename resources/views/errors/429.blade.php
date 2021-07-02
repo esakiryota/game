3e96e11c3fae126b8f173f29f281a22acfcef9f9
江崎亮太 <esaki1217@gmail.com>
2021-07-03 00:11:45 +0900

    error pageの追加

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.default')
+@section('content')
+<div class="" style="position: relative">
+  <img src="/img/serif.png" width="500px">
+  <div style="position: absolute; top: 30%; left: 30px; font-size: 50px; color: white;">
+    ページがありません。ごめんなさい。
+  </div>
+</div>
+<div class="" style="position: relative">
+  <img src="/img/anigif.gif" width="100px" style="right: 50px; position: absolute; left: 0px;">
+</div>
+@endsection
